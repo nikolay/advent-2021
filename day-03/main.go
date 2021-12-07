54f2421970e7cd2b1ba280e30ecf1e0818aea7ef
Nikolay Kolev <nikolay@users.noreply.github.com>
2021-12-07 02:46:29 -0800

    refactor: flexible bit size

--- day-03/main.go
+++ day-03/main.go
@@ -17,48 +17,35 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	count := uint(0)
-	freq := make(map[uint]uint, 5)
+	freq := make([]uint, 0, 64)
+	bitSize := int(0)
 	for scanner.Scan() {
-		bits, err := strconv.ParseUint(scanner.Text(), 2, 5)
+		bits := scanner.Text()
+		if bitSize == 0 {
+			bitSize = len(bits)
+			freq = freq[0:bitSize]
+		}
+		decimal, err := strconv.ParseUint(bits, 2, bitSize)
 		if err != nil {
 			log.Fatal(err)
 		}
+		for i := 0; i < bitSize; i++ {
+			if decimal&(1<<i) != 0 {
+				freq[i]++
+			}
+		}
 		count++
-		if bits&1 != 0 {
-			freq[0]++
-		}
-		if bits&2 != 0 {
-			freq[1]++
-		}
-		if bits&4 != 0 {
-			freq[2]++
-		}
-		if bits&8 != 0 {
-			freq[3]++
-		}
-		if bits&16 != 0 {
-			freq[4]++
-		}
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	gamma := 0
-	if freq[0]<<1 > count {
-		gamma |= 1
+	gamma, epsilon := 0, 0
+	for i := 0; i < bitSize; i++ {
+		if freq[i]<<1 > count {
+			gamma |= 1 << i
+		} else {
+			epsilon |= 1 << i
+		}
 	}
-	if freq[1]<<1 > count {
-		gamma |= 2
-	}
-	if freq[2]<<1 > count {
-		gamma |= 4
-	}
-	if freq[3]<<1 > count {
-		gamma |= 8
-	}
-	if freq[4]<<1 > count {
-		gamma |= 16
-	}
-	epsilon := gamma ^ 0x1f
 	fmt.Println(gamma * epsilon)
 }
