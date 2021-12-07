46b0b110e9f175aba0edba1fb5518ee3cbdc74b5
Nikolay Kolev <nikolay@users.noreply.github.com>
2021-12-07 03:29:19 -0800

    feat: add a runner shell script

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+usage () {
+	echo "USAGE: $0 <day> <part>"
+	exit 1
+}
+
+main () {
+	if [ $# != 2 ]; then
+		usage
+	fi
+	local -ri day="$1"
+	local -ri part="$2"
+
+	if [[ -z ${day} || -z ${part} ]]; then
+		usage
+	fi
+
+	pushd "./days/$(printf '%02d' ${day})" 1>/dev/null
+	go run "part${part}.go"
+	popd 1>/dev/null
+}
+
+main "$@"
