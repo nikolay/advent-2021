280b0f805d86a16139f1cdcc90f01ddf9cef4025
Nikolay Kolev <nikolay@users.noreply.github.com>
2021-12-08 16:44:08 -0800

    feat: day-01 part-02

---
+++ days/01/part2.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+const bufferSize = 3
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	last := -1
+	count := 0
+	buffer := make([]int, 0, bufferSize)
+	for scanner.Scan() {
+		num, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		buffer = append(buffer, num)
+		if len(buffer) < bufferSize {
+			continue
+		}
+		sum := 0
+		for _, v := range buffer {
+			sum += v
+		}
+		buffer = buffer[1:]
+		if last != -1 && sum > last {
+			count++
+		}
+		last = sum
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(count)
+}
